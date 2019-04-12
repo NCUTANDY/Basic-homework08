3a64bd745d9d2f4235605882bc3b967645a1590c
NCUTANDY <48341523+NCUTANDY@users.noreply.github.com>
2019-04-12 08:28:36 +0800

    Add files via upload

---
+++ 08.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int num1,num2,num; 
+    printf("請輸入數字上限:");
+	scanf("%d",&num); 
+    for(num1=0;num1<=num;num1++) 
+{
+    for(num2=2;num2<=num1;num2++) 
+    if(num1%num2==0) 
+    break; 
+    if(num1==num2) 
+    printf("%d,",num2);
+}
+	system("pause");
+	return 0;
+}
+
